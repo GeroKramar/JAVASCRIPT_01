7f2d3571c0d6f5b7c623397493f4fd1e990d10a7
Geronimo Kramar <132961854+GeroKramar@users.noreply.github.com>
2023-10-24 22:40:54 -0300

    Push ej 3

---
+++ Ejercicio1_Script.js
@@ -0,0 +1,6 @@
+// Escribir un algoritmo en el cual se consulte al usuario que ingrese ¿cómo está el día de
+// hoy? (soleado, nublado, lloviendo). A continuación, mostrar por pantalla un mensaje que
+// indique “El día de hoy está ...”, completando el mensaje con el dato que ingresó el usuario.
+
+var imp = prompt("Como está el día hoy?");
+alert("el dia está " + imp);
---
+++ Ejercicio3_Script.js
@@ -0,0 +1,10 @@
+// Escriba un programa en donde se pida la edad del usuario. Si el usuario es mayor de
+// edad se debe mostrar un mensaje indicándolo. 
+
+var rta = prompt("Ingrese su edad");
+
+if(rta >= 18){
+    alert("usted es Mayor de edad");
+} else{
+    alert("Usted es un wachin");
+}
